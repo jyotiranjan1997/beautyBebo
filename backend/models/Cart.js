0a32f5c5914b1987e0875164f00d36f278826b33
jyotiranjan1997 <manomohanty281@gmail.com>
2022-12-18 15:47:18 +0530

    backend data for pretty pink

---
+++ backend/models/Cart.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    productId: { type: String },
+    quantity: { type: Number, default: 1 },
+  },
+
+  { timestamps: true }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
---
+++ backend/routes/carts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  addToCart,
+  updateCart,
+  deleteFromCart,
+  getUserCart,
+} = require("../controllers/cartController");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  CartMiddleWare,
+} = require("../middlewares/verifyToken");
+
+const cartRoutes = express.Router();
+
+/* ADD TO CART */
+cartRoutes.post("/add", CartMiddleWare, addToCart);
+
+/* UPDATE */
+cartRoutes.patch("/:id", CartMiddleWare, updateCart);
+
+/* DELETE */
+cartRoutes.delete("/:id", CartMiddleWare, deleteFromCart);
+
+/* GET USER CART BY USERID */
+cartRoutes.get("/", CartMiddleWare, getUserCart);
+
+module.exports = cartRoutes;
